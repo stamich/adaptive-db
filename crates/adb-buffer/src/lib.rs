72a96e44af34af110d1c02387f15d1bb3e940ac5
Michal Stawarski <michal.stawarsi@codeswarm.io>
2026-09-08 19:55:55 +0200

    Implements BufferPool

--- crates/adb-buffer/src/lib.rs
+++ crates/adb-buffer/src/lib.rs
@@ -1,11 +1,11 @@
 //! Lib module for the adb-buffer crate.
 //!
-// pub mod buffer_pool;
+pub mod buffer_pool;
 pub mod error;
 pub mod file_store;
 pub mod page_store;
 
-// pub use buffer_pool::BufferPool;
+pub use buffer_pool::BufferPool;
 pub use error::BufferError;
 pub use file_store::FilePageStore;
 pub use page_store::PageStore;
